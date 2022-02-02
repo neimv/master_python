e63863d9ec3ca746fa6d1295f506924e0d684e13
Neimv Zephon Zatara Lyra <neimv.zatara@gmail.com>
2022-02-01 18:25:46 -0600

    stacks

---
+++ data/data_structure/stack.py
@@ -0,0 +1,44 @@
+
+from nodes import Node
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.size = 0
+
+    def push(self, data):
+        node = Node(data)
+
+        if self.top is None:
+            node.next = self.top
+            self.top = node
+        else:
+            self.top = node
+
+        self.size += 1
+
+    def pop(self):
+        if self.top:
+            data = self.top.data
+            self.size -= 1
+
+            if self.top.next:
+                self.top = self.top.next
+            else:
+                self.top = None
+
+            return data
+        else:
+            return "The stack is empty"
+
+    def peek(self):
+        if self.top:
+            return self.top.data
+        else:
+            return "The stack is empty"
+
+    def clear(self):
+        while self.top:
+            self.pop()
+
