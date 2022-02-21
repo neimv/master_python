0b7de34ca57182af7dd24c6c5af73acafbd301aa
Neimv Zephon Zatara Lyra <neimv.zatara@gmail.com>
2022-02-20 18:03:40 -0600

    500

--- web/fastapi-learn/main.py
+++ web/fastapi-learn/main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+
+from typing import Optional
+
+from pydantic import BaseModel
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+
+
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hain: Optional[str] = None
+    is_married: Optional[bool]= None
 
 
 @app.get('/')
 def home():
     return {"hello": "World"}
+
+
+# Request and response body
+@app.post('/person/new')
+def create_person(person: Person = Body(...)):
+
+    return Person
