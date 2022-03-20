09c2a77883e645e2a251f9daf19a4d397967b7b5
Neimv Zephon Zatara Lyra <neimv.zatara@gmail.com>
2022-03-20 16:03:43 -0600

    in test flask

--- web/flask-platzi/main.py
+++ web/flask-platzi/main.py
@@ -1,9 +1,17 @@
 
-from flask import Flask, request, make_response, redirect, render_template
+import unittest
+
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
+app.config['SECRET_KEY'] = 'leonidas'
 
 todos = [
     'Comprar cafe',
@@ -12,25 +20,49 @@ todos = [
 ]
 
 
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/healthcheck'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
-@app.route('/healthcheck')
-def healtcheck():
-    user_ip = request.cookies.get('user_ip')
+@app.route('/healthcheck', methods=['GET', 'POST'])
+def healthcheck():
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form,
+        'username': username,
     }
 
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usuario registrado con exito')
+
+        return redirect(url_for('index'))
+
     return render_template('hello.html', **context)
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 
 @app.errorhandler(404)
