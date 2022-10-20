cff9dd46f548afa0ec652ab73d7568f7d6ca7905
neimv <neimv.zatara@gmail.com>
2022-10-20 18:30:57 -0500

    creating expos

---
+++ general/expos/main.py
@@ -0,0 +1,21 @@
+
+import argparse
+
+
+def write_file():
+    pass
+
+
+def read_file():
+    pass
+
+
+def main(argument):
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Basic project to files")
+    parser.add_argument("filename")
+
+    main(parser)
